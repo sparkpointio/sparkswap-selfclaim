11f7cc8d9a32e93f954356a2cfc11d4c70c3bf06
Medard Mandane <medardm13@gmail.com>
2023-12-11 17:30:48 +0800

    Add ability to only seed test data on specific tables using --table option

--- src/database/refreshDatabase.ts
+++ src/database/refreshDatabase.ts
@@ -2,6 +2,8 @@ import prisma from "@/library/prisma";
 import {Prisma} from ".prisma/client";
 
 const tableNames = Object.values(Prisma.ModelName);
+type Keys = keyof typeof tableNames;
+export type TableNames = (typeof tableNames)[Keys];
 // exclude the constants or prerquisite values
 const excludedTableNames = [
   'Role'
