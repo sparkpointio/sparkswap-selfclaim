7a6f7334009e9c78b6fec392198b74e3e31fbfa6
clowreed <clowreed@gmail.com>
2023-12-18 21:09:39 +0800

    Wrap the pages in Layout component to use the Navbar

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,16 +1,24 @@
-import React, {useState} from "react";
-import {useAddress, Web3Button,} from "@thirdweb-dev/react";
-import {NavigationMenu} from "@/resources/components/NavigationMenu";
-import {formatInputRecipients, getMerkleInfo} from "@/library/utils/merkle.utils";
-import {useApproveToken, useTokenAllowance, useTokenContract} from "@/library/hooks/useToken";
+import React, { useState } from "react";
+import { useAddress, Web3Button } from "@thirdweb-dev/react";
+import { NavigationMenu } from "@/resources/components/NavigationMenu";
+import Layout from "@/resources/components/layout/Layout";
+import {
+  formatInputRecipients,
+  getMerkleInfo,
+} from "@/library/utils/merkle.utils";
+import {
+  useApproveToken,
+  useTokenAllowance,
+  useTokenContract,
+} from "@/library/hooks/useToken";
 import tokens from "@/library/constants/tokens";
 import contracts from "@/library/constants/contracts";
 import selfClaimFactory from "@/library/hooks/useSelfClaimFactory";
-import {erc20Abi} from "abitype/abis";
+import { erc20Abi } from "abitype/abis";
 import moment from "moment";
 import airdropHooks from "@/library/hooks/api-calls/useAirdrop";
-import {faker} from "@faker-js/faker";
-import {useAuthUser} from "@/library/hooks/useAuthUser";
+import { faker } from "@faker-js/faker";
+import { useAuthUser } from "@/library/hooks/useAuthUser";
 
 export default function Home() {
   // User's connected wallet address
@@ -27,105 +35,114 @@ export default function Home() {
   const rewardToken = useTokenContract(rewardTokenAddress);
 
   // Event listeners for Token approval events
-  const {approve} = useApproveToken()
-  const {allowance} = useTokenAllowance(rewardToken.contract, contracts.selfClaimFactory.address)
+  const { approve } = useApproveToken();
+  const { allowance } = useTokenAllowance(
+    rewardToken.contract,
+    contracts.selfClaimFactory.address
+  );
 
   // Use contract for selfclaim airdrop contract
-  const {createSelfClaim, receipt} = selfClaimFactory.useCreateSelfClaim()
+  const { createSelfClaim, receipt } = selfClaimFactory.useCreateSelfClaim();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-background3 mt-12">
-      <NavigationMenu/>
-      <h1 className="text-5xl font-bold tracking-tight text-text1">
-        Welcome, Airdrop Creators
-      </h1>
-      <h2 className="text-3xl tracking-tight text-text2">
-        Create a Self-Claim Airdrop
-      </h2>
-      <div className="w-5/6 mx-auto">
-        <div className="mb-4">
-          <label htmlFor="name" className="block mb-2 font-medium text-text1">
-            Token Address
-          </label>
-          <input
-            type="text"
-            id="rewardTokenAddress"
-            className="w-full px-3 py-2 border border-accent2 rounded-md text-text2 bg-background2"
-            value={rewardTokenAddress}
-            onChange={(e) => setRewardTokenAddress(e.target.value)}
-            placeholder="Enter token address..."
-          />
-        </div>
+    <Layout>
+      <>
+        <h1 className="text-5xl font-bold tracking-tight text-text1">
+          Welcome, Airdrop Creators
+        </h1>
+        <h2 className="text-3xl tracking-tight text-text2">
+          Create a Self-Claim Airdrop
+        </h2>
+        <div className="w-5/6 mx-auto">
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2 font-medium text-text1">
+              Token Address
+            </label>
+            <input
+              type="text"
+              id="rewardTokenAddress"
+              className="w-full px-3 py-2 border border-accent2 rounded-md text-text2 bg-background2"
+              value={rewardTokenAddress}
+              onChange={(e) => setRewardTokenAddress(e.target.value)}
+              placeholder="Enter token address..."
+            />
+          </div>
 
-        <div className="mb-4">
-          <label
-            htmlFor="message"
-            className="block mb-2 font-medium text-text1"
-          >
-            List of Addresses in CSV
-          </label>
-          <textarea
-            id="message"
-            className="w-full px-3 py-2 border border-accent2 rounded-md text-text2 bg-background2"
-            rows={4}
-            value={recipients}
-            onChange={(e) => setRecipients(e.target.value)}
-          ></textarea>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block mb-2 font-medium text-text1"
+            >
+              List of Addresses in CSV
+            </label>
+            <textarea
+              id="message"
+              className="w-full px-3 py-2 border border-accent2 rounded-md text-text2 bg-background2"
+              rows={4}
+              value={recipients}
+              onChange={(e) => setRecipients(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="mb-4 w-full">
+            <p>
+              Current Allowance: {allowance?.displayValue} {allowance?.symbol}
+            </p>
+            <Web3Button
+              connectWallet={{
+                btnTitle: "Approve selfclaim",
+                modalTitle: "Please connect your wallet first",
+              }}
+              contractAddress={rewardTokenAddress}
+              contractAbi={erc20Abi}
+              action={async (contract) => {
+                await approve(
+                  contract,
+                  contracts.selfClaimFactory.address,
+                  "1000"
+                );
+              }}
+              onSuccess={(result) => {
+                alert("tokens approved");
+              }}
+              onError={(error) =>
+                alert("Something went wrong (Approving tokens)!")
+              }
+              className="bg-accent1 hover:bg-accent2"
+            >
+              Approve selfclaim
+            </Web3Button>
+          </div>
+          <div className="mb-4 w-full">
+            <Web3Button
+              connectWallet={{
+                btnTitle: "Create Airdrop",
+                modalTitle: "Please connect your wallet first",
+              }}
+              contractAddress={contracts.selfClaimFactory.address}
+              contractAbi={contracts.selfClaimFactory.ABI}
+              action={async (contract) => {
+                const { merkleRecipientList, totalAmountValue } =
+                  formatInputRecipients(recipients, rewardToken.decimals);
+                const merkleInfo = getMerkleInfo(merkleRecipientList);
+                await createSelfClaim(contract, {
+                  feeTokenAddress: selfClaimFactory.feeToken.primary.address,
+                  rewardTokenAddress: rewardTokenAddress,
+                  merkleRoot: merkleInfo.merkleRoot,
+                  expiry: moment().add(10).unix().toString(),
+                  totalAmount: totalAmountValue ?? "0",
+                });
+              }}
+              onSuccess={(result) => alert("Created airdrop submitted")}
+              onError={(error) => {
+                console.error(error);
+                alert(error);
+              }}
+              className="bg-accent1 hover:bg-accent2"
+            >
+              Create airdrop
+            </Web3Button>
+          </div>
         </div>
-        <div className="mb-4 w-full">
-          <p>
-            Current Allowance: {allowance?.displayValue} {allowance?.symbol}
-          </p>
-          <Web3Button
-            connectWallet={{
-              btnTitle: "Approve selfclaim",
-              modalTitle: 'Please connect your wallet first'
-            }}
-            contractAddress={rewardTokenAddress}
-            contractAbi={erc20Abi}
-            action={async (contract) => {
-              await approve(contract, contracts.selfClaimFactory.address, '1000');
-            }}
-            onSuccess={(result) => {
-              alert('tokens approved')
-            }}
-            onError={(error) =>
-              alert("Something went wrong (Approving tokens)!")
-            }
-            className="bg-accent1 hover:bg-accent2"
-          >
-            Approve selfclaim
-          </Web3Button>
-        </div>
-        <div className="mb-4 w-full">
-          <Web3Button
-            connectWallet={{
-              btnTitle: "Create Airdrop",
-              modalTitle: 'Please connect your wallet first'
-            }}
-            contractAddress={contracts.selfClaimFactory.address}
-            contractAbi={contracts.selfClaimFactory.ABI}
-            action={async (contract) => {
-              const {merkleRecipientList, totalAmountValue} = formatInputRecipients(recipients, rewardToken.decimals);
-              const merkleInfo = getMerkleInfo(merkleRecipientList);
-              await createSelfClaim(contract, {
-                feeTokenAddress: selfClaimFactory.feeToken.primary.address,
-                rewardTokenAddress: rewardTokenAddress,
-                merkleRoot: merkleInfo.merkleRoot,
-                expiry: moment().add(10).unix().toString(),
-                totalAmount: totalAmountValue ?? '0'
-              })
-            }}
-            onSuccess={(result) => alert("Created airdrop submitted")}
-            onError={(error) => {
-              console.error(error)
-              alert(error)
-            }}
-            className="bg-accent1 hover:bg-accent2"
-          >
-            Create airdrop
-          </Web3Button>
-        </div>
-      </div>
-    </main>
+      </>
+    </Layout>
   );
 }
