0b8d260412e01e3e3a8a1f0208d4bf822f1c5c72
Medard Mandane <medardm13@gmail.com>
2023-12-20 22:22:29 +0800

    Move generating of test recipients to test helper and save generated mock recipients to file

--- src/database/factory/airdrop.factory.ts
+++ src/database/factory/airdrop.factory.ts
@@ -2,7 +2,6 @@ import {faker} from "@faker-js/faker";
 import airdropModel from "@/library/models/airdrop.model";
 import {Airdrop, Prisma, User} from "@prisma/client";
 import userFactory from "@/database/factory/user.factory";
-import {ethers} from "ethers";
 import {formatInputRecipients, getMerkleInfo} from "@/library/utils/merkle.utils";
 import {Amount} from "@thirdweb-dev/sdk";
 import {MerkleDistributorInfo} from "@medardm/merkle-distributor";
@@ -15,6 +14,7 @@ import {
 import {TokenContract} from "@/library/hooks/useToken";
 import tokens from "@/library/constants/tokens";
 import {Address} from "@thirdweb-dev/react";
+import {generateTestRecipients} from "@/library/helpers/test.helper";
 
 export type AirdropWithMerkle = {
   airdrop: Airdrop;
@@ -78,7 +78,9 @@ const airdropFactory = {
 
       // save generated merkle info
       if (config.merkle.createFile) {
-        fs.writeFileSync(`./generated-merkle/${faker.string.uuid()}.json`, JSON.stringify(merkleInfo));
+        const uuid = faker.string.uuid();
+        fs.writeFileSync(`./generated-merkle/${uuid}-data.txt`, mockRecipientsInput);
+        fs.writeFileSync(`./generated-merkle/${uuid}-merkle.json`, JSON.stringify(merkleInfo));
       }
 
       if (!creator) {
@@ -105,19 +107,6 @@ const airdropFactory = {
     }
     return records
   }
-}
-
-/**
- * Helpers
- */
-export const generateTestRecipients = (quantity = 1) => {
-  const parts = Array.from({length: quantity}, (_, i) =>
-    `${ethers.Wallet.createRandom().address}, ` +
-    `${faker.string.numeric({length: {min: 1, max: 2}, allowLeadingZeros: false})}` +
-    `${(i + 1) === quantity ? '' : '\n'}`
-  );
-
-  return parts.join('');
 }
 
 export default airdropFactory
